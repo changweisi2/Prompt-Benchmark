e83cc9818512ce186e40f101bea5726983fdb69c
CADICAT <2453873938@qq.com>
2025-12-09 16:29:05 +0800

    initialize GAOKAO repo

---
+++ field_classificition.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Field(Enum):
+    MathematicalReasoning = "mathematical_reasoning"
+    LogicalReasoning = "logical_reasoning"
+    Lang_Comp_and_Produc = "lang_comp_and_produc"
+    Scientific_Inquiry = "scientific_inquiry"
+    Sociocultural_Understanding = "sociocultural_understanding"
+    Data_and_StatisticalLiteracy = "data&statistical_literacy"
+    Commonsense_and_WorldKnowledge = "commonsense_and_world_knowledge"
+    Creative_and_Openended_Questions = "creative_and_openended_questions"
